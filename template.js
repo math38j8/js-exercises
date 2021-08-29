7e2e1f121ac632ad6e8fc31ffb165a71ea2b0428
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-08-29 15:02:58 +0200

    template literals done

---
+++ template.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const myName = "Mathilde";
+const pet = "hamster";
+const petName = "Cordelia";
+
+console.log(`My name is ${myName}.
+I have a ${pet} called ${petName}.`);
+
+console.log(`My name is ${myName}. \nI have a ${pet} called ${petName}.`)
