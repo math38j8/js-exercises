68adfd0e22354e346615b4975456966025d65e7f
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-09-02 11:58:21 +0200

    correction in the js file

--- forloop.js
+++ forloop.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//__________________________
 //0-9
 
 // for( let counter=0; counter <10; counter++) {
@@ -7,7 +7,7 @@
 // }
 
 
-
+//__________________________
 //1-10
 
 // for( let counter=1; counter <11; counter++) {
@@ -15,7 +15,7 @@
 // }
 
 
-
+//__________________________
 //From 10-0 and after 0 console.log the text "liftoff"
 
 // for( let counter=10; counter >=0; counter--) {
@@ -25,6 +25,7 @@
 // console.log("liftoff");
 
 
+//__________________________
 //From 1 to 19 but only counts odd numbers (no if-statements, change the increment)
 
 // for( let counter=1; counter <20; counter+=2) {
@@ -32,12 +33,15 @@
 // }
 
 
+//__________________________
 //From 1 to 16777216, but doubling each time(2,4, 8, 16)
 
 // for( let counter=1; counter <16777216; counter*=2) {
 //     console.log(counter);
 //     }
 
+
+//__________________________
 //From 111 to 138 in steps of 3
 
 // for (let i =111; i <= 138; i += 3){
@@ -45,6 +49,7 @@
 // }
 
 
+//__________________________
 //From 100 down to 0 in steps of 10
 
 // for (let i =100; i >= 0; i -= 10){
