c5173663adcd64fcaa453dd9ca19e8282ce904b8
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-09-02 11:56:37 +0200

    for loops exercises

---
+++ forloop.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//0-9
+
+// for( let counter=0; counter <10; counter++) {
+//     console.log(counter);
+// }
+
+
+
+//1-10
+
+// for( let counter=1; counter <11; counter++) {
+//     console.log(counter);
+// }
+
+
+
+//From 10-0 and after 0 console.log the text "liftoff"
+
+// for( let counter=10; counter >=0; counter--) {
+//     console.log(counter);
+   
+// }
+// console.log("liftoff");
+
+
+//From 1 to 19 but only counts odd numbers (no if-statements, change the increment)
+
+// for( let counter=1; counter <20; counter+=2) {
+// console.log(counter);
+// }
+
+
+//From 1 to 16777216, but doubling each time(2,4, 8, 16)
+
+// for( let counter=1; counter <16777216; counter*=2) {
+//     console.log(counter);
+//     }
+
+//From 111 to 138 in steps of 3
+
+// for (let i =111; i <= 138; i += 3){
+//     console.log(i);
+// }
+
+
+//From 100 down to 0 in steps of 10
+
+// for (let i =100; i >= 0; i -= 10){
+//     console.log(i);
+// }
