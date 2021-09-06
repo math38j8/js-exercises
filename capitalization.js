3a7a8beb88f0bdf3c3229fb84d49c8c0501eae8f
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-09-06 14:44:08 +0200

    capitalization with parameters

--- capitalization.js
+++ capitalization.js
@@ -1,7 +1,22 @@
 "use strict";
 
-const firstString = "peter";
+//const firstString = "peter";
 
-let firstName = firstString[0].toUpperCase() + firstString.substring(1);
-console.log(firstName);
+//let firstName = firstString[0].toUpperCase() + firstString.substring(1);
+//console.log(firstName);
+
+
+
+//const str = "peter";
+
+capitalize("matHilDe");
+
+function capitalize(str) {
+    let firstLetter = str[0].toUpperCase();
+    let restName = str.substring(1).toLowerCase();
+    let newName = firstLetter + restName;
+    console.log(newName);
+    console.log(firstLetter);
+    console.log(restName);
+};
 
