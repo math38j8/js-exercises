df55fc206badb9186d6728a325e7c268c0ae5224
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-09-07 11:55:21 +0200

    rgbToHex

--- colorconvert.js
+++ colorconvert.js
@@ -1,6 +1,8 @@
+//HEX to RGB
+
 String.prototype.convertToRGB = function(){
-    var aRgbHex = this.match(/.{1,2}/g);
-    var aRgb = [
+    let aRgbHex = this.match(/.{1,2}/g);
+    let aRgb = [
         parseInt(aRgbHex[0], 16),
         parseInt(aRgbHex[1], 16),
         parseInt(aRgbHex[2], 16)
@@ -9,3 +11,16 @@ String.prototype.convertToRGB = function(){
 }
 
 console.log('c0ffee'.convertToRGB()); //[ 192, 255, 238 ]
+
+
+
+//RGB to HEX
+function ColorToHex(color) {
+    let hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+  }
+  
+  function ConvertRGBtoHex(red, green, blue) {
+    return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+  }
+  console.log(ConvertRGBtoHex(192, 255, 238));
